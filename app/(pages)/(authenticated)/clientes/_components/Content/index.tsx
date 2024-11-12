387ae08886b0d1088b2fcc0ea4b8034e1d57cc67
Guilherme Corso <guilhermemaffeidev@gmail.com>
2024-11-12 14:30:47 -0300

    changes

--- app/(pages)/(authenticated)/clientes/_components/Content/index.tsx
+++ app/(pages)/(authenticated)/clientes/_components/Content/index.tsx
@@ -1,14 +1,31 @@
 "use client";
 
 import { Input } from "@/app/_components/ui/Input";
+import { Label } from "@/app/_components/ui/Label";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/app/_components/ui/Select";
 
 function Content() {
   return (
     <div className="max-w-[1700px] m-auto grid grid-rows-[auto_1fr] h-full gap-4">
-      <div className="h-full grid grid-cols-12 shadow-md bg-white p-4">
-        <div className="col-span-3">
-          <div>Nome do cliente</div>
-          <Input variant="filled" />
+      <div className="h-full grid grid-cols-12 gap-4 shadow-md bg-white p-4">
+        <div className="col-span-12 md:col-span-3">
+          <Label>Nome do cliente</Label>
+          <Input placeholder="Digite o nome do cliente" variant="filled" />
+        </div>
+
+        <div className="col-span-12 md:col-span-2">
+          <Label>Status</Label>
+
+          <Select>
+            <SelectTrigger className="w-full">
+              <SelectValue placeholder="Selecione o status" />
+            </SelectTrigger>
+
+            <SelectContent>
+              <SelectItem value="light">Ativo</SelectItem>
+              <SelectItem value="dark">Inativo</SelectItem>
+            </SelectContent>
+          </Select>
         </div>
       </div>
       <div className="h-full"></div>
