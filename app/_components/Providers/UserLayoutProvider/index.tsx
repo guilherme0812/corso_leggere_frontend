7b9de85bd6dd69eb3b8bcbb9db118f35ebb9a267
guilherme <guilherme09112001@gmail.com>
2025-10-22 22:13:08 +0200

    changes

--- app/_components/Providers/UserLayoutProvider/index.tsx
+++ app/_components/Providers/UserLayoutProvider/index.tsx
@@ -1,9 +1,10 @@
 "use client";
 
-import { LuCalendar, LuCircleDollarSign, LuFileText, LuPieChart, LuUsers } from "react-icons/lu";
+import { LuCalendar, LuCircleDollarSign, LuFileText, LuNewspaper, LuPieChart, LuUsers } from "react-icons/lu";
 import PrimaryLayout from "../../Layout/PrimaryLayout";
 import { ReactNode } from "react";
 import { usePathname } from "next/navigation";
+import { MdOutlineBalance } from "react-icons/md";
 
 function UserLayoutProvider({ children }: { children: ReactNode }) {
   const pathname = usePathname();
@@ -17,12 +18,12 @@ function UserLayoutProvider({ children }: { children: ReactNode }) {
           link: "/painel",
           selected: pathname == "/painel",
         },
-        {
-          icon: <LuFileText />,
-          label: "Processos",
-          link: "/processos",
-          selected: pathname == "/processos",
-        },
+        // {
+        //   icon: <LuFileText />,
+        //   label: "Processos",
+        //   link: "/processos",
+        //   selected: pathname == "/processos",
+        // },
         {
           icon: <LuUsers />,
           label: "Clientes",
@@ -30,17 +31,29 @@ function UserLayoutProvider({ children }: { children: ReactNode }) {
           selected: pathname == "/clientes",
         },
         {
-          icon: <LuCircleDollarSign />,
-          label: "Financeiro",
-          link: "/financeiro",
-          selected: pathname == "/financeiro",
+          icon: <MdOutlineBalance />,
+          label: "Advogados",
+          link: "/advogados",
+          selected: pathname == "/advogados",
         },
         {
-          icon: <LuCalendar />,
-          label: "Calendario",
-          link: "/calendario",
-          selected: pathname == "/calendario",
+          icon: <LuNewspaper />,
+          label: "Documentos",
+          link: "/gerador-de-documentos",
+          selected: pathname == "/gerador-de-documentos",
         },
+        // {
+        //   icon: <LuCircleDollarSign />,
+        //   label: "Financeiro",
+        //   link: "/financeiro",
+        //   selected: pathname == "/financeiro",
+        // },
+        // {
+        //   icon: <LuCalendar />,
+        //   label: "Calendario",
+        //   link: "/calendario",
+        //   selected: pathname == "/calendario",
+        // },
       ]}
     >
       {children}
